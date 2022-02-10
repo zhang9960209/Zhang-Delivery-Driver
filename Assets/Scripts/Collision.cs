f2a0840cfe0a0a6bd29045b16f62ba12a31bb32f
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-02-10 15:20:06 -0500

    Add asset to project

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -8,4 +8,9 @@ public class Collision : MonoBehaviour
     {
         Debug.Log("shit!");
     }
+
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("WTF is that?!");
+    }
 }
