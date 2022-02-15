0ca01a765e1ffd71421667ec9f76cada75ea9fdf
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-02-15 15:18:54 -0500

    How to Destroy Objects

--- Assets/Scripts/Delivery.cs
+++ Assets/Scripts/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+    bool blGetPackage;
+    [SerializeField] float flDetroyDelay = 0.5f;
     void OnCollisionEnter2D(Collision2D other) 
     {
         Debug.Log("shit!");
@@ -12,13 +14,16 @@ public class Delivery : MonoBehaviour
     void OnTriggerEnter2D(Collider2D other) 
     {
         // if the thing triggered in the pkg, print out to console
-        if (other.tag == "Pkg")
+        if (other.tag == "Pkg" && !blGetPackage)
         {
              Debug.Log("Package picked up!");
+             blGetPackage = true;
+             Destroy(other.gameObject, flDetroyDelay);
         }
-        if (other.tag == "Client")
+        if (other.tag == "Client" && blGetPackage)
         {
             Debug.Log("Package Delivered");
+            blGetPackage = false;
         }
     }
 }
--- Assets/Scripts/FollowCamera.cs
+++ Assets/Scripts/FollowCamera.cs
@@ -6,7 +6,7 @@ public class FollowCamera : MonoBehaviour
 {
     // Camera position should be the same as the car's position
     [SerializeField] GameObject thingToFollow;
-    void LateUpdate()
+    void Update()
     {
         transform.position = thingToFollow.transform.position + new Vector3 (0,0,-10);
     }
